53dbc1bdca33fad66a6dda1a8785ced6b65eaa5c
darklion998 <tai.cnpmk06@gmail.com>
2015-07-19 09:08:12 +0700

    Collision
    chuyen sang dung collision nay

---
+++ Megaman/CollisionInfo.h
@@ -0,0 +1,25 @@
+﻿//-----------------------------------------------------------------------------
+// File: CCollisionInfo.h
+//
+// Desc: Định nghĩa lớp CItem có các dạng như Score, Mana, Power
+//
+//-----------------------------------------------------------------------------
+#ifndef _COLLISION_IFNO_H_
+#define _COLLISION_IFNO_H_
+
+#include "GameObject.h"
+#include "CCollision.h"
+
+struct CollisionInfo
+{
+public:
+	GameObject*	_object;
+	CDirection		_direction;
+	float			_timeCollide;
+
+	CollisionInfo();
+	CollisionInfo(GameObject* object, CDirection direction, float timeCollide);
+};
+
+
+#endif // !_COLLISION_IFNO_H_
--- Megaman/Global.h
+++ Megaman/Global.h
@@ -1,9 +1,15 @@
 ﻿#pragma once
 
+#include <d3dx9.h>
 #include <stdio.h>
+#include <vector>
+#include <dinput.h>
 #include <stdarg.h>
 #include <windows.h>
 #include <cmath>
+
+using namespace std;
+
 
 #define GAME_NAME "Megaman"
 #define SCREEN_WIDTH 300
@@ -16,3 +22,22 @@
 
 // ID đối tượng
 #define ID_LAND 1
+
+typedef LPDIRECT3DDEVICE9		Device;
+typedef LPDIRECT3DSURFACE9		Surface;
+typedef LPDIRECT3DTEXTURE9		Texture;
+typedef LPD3DXSPRITE			Sprite;
+typedef LPD3DXFONT				SpriteFont;
+typedef LPDIRECT3D9				DirectX9;
+typedef D3DXIMAGE_INFO			ImageInfo;
+typedef D3DXVECTOR2				Vector2;
+typedef D3DXVECTOR3				Vector3;
+typedef D3DXVECTOR4				Vector4;
+typedef LPDIRECTINPUT8			Input;
+typedef LPDIRECTINPUTDEVICE8	InputDevice;
+typedef DIDEVICEOBJECTDATA		DeviceObjectData;
+typedef D3DXMATRIX				Matrix;
+typedef D3DCOLOR				Color;
+typedef RECT					Rect;
+
+#define FONT_SIZE	8
