45cee5c75e310abe2d82bfcbca8261a5eb8eea46
hyubyn <nguyenvuhuy0407@gmail.com>
2015-08-11 22:43:16 +0700

    Boom Enemy

---
+++ Megaman/CEnemyBoom.h
@@ -0,0 +1,62 @@
+﻿//-----------------------------------------------------------------------------
+// File: CEnemyBoom.h
+//
+// Desc: Định nghĩa lớp CEnemyBoom
+//
+//-----------------------------------------------------------------------------
+#ifndef _CENEMY_BOOM_H_
+#define _CENEMY_BOOM_H_
+
+#include "CGameObject.h"
+#include "CEnemy.h"
+#include "ResourceManager.h"
+#include "CEnemyBoomBullet.h"
+#include <stdlib.h>
+#include "ExplodingEffect.h"
+#include "Megaman.h"
+
+enum class ENEMYBOOM_STATE
+{
+	FLYING,
+	EXPLODING,
+	DELAY
+};
+
+class CEnemyBoom : public CEnemy
+{
+public:
+	CEnemyBoom(int id, int typeID, CSprite spriteEnemyBoom, CSprite spriteExplodingEffect, float vy, D3DXVECTOR2 positionBegin, int dame, int blood = 2, int score = SCORE_DEFAULT);
+	~CEnemyBoom();
+
+	int Initlize() override;
+
+	void Update(CTimer* gameTime, Megaman* rockman) override;
+
+	void Render(CTimer* gameTime, MGraphic* graphics) override;
+
+	void UpdateBox() override;
+
+	void OnCollideWith(CGameObject* gameObj, CDirection normalX, CDirection normalY, float deltaTime) override;
+
+	CEnemyBoom* ToValue() override;
+
+	vector<CBullet*> Fire() override;
+
+private:
+	// Tấn công Rockman
+	void AttackRockman(float deltaTime);
+	//Lưu giữ vận tốc ban đầu
+	D3DXVECTOR2 _vBegin;
+	// Cho biết trạng thái là đang tấn công hay không?
+	ENEMYBOOM_STATE _state;
+	// Danh sách đạn bắn ra
+	vector<CBullet*> _lstBullet;
+	// Thời gian nghỉ giữa 2 tấn công. Đơn vị là Tick
+	int _timeDelayAttackRemain;
+	//Đối tượng tạo hiệu ứng nổ
+	CExplodingEffect* _explodingEffect;
+	//Lưu lại vị trí xuất hiện ban đầu
+	D3DXVECTOR2 _positionBegin;
+};
+
+#endif // !_CENEMY_BOOM_H_
---
+++ Megaman/CEnemyMachineAutoBullet.cpp
@@ -0,0 +1,53 @@
+﻿#include "CEnemyMachineAutoBullet.h"
+
+
+CEnemyMachineAutoBullet::CEnemyMachineAutoBullet(int id, int typeID, CSprite spriteBullet, int dame, float v0, D3DXVECTOR2 beginPosition, float angleFly)
+	: CBullet(id, typeID, spriteBullet, dame, D3DXVECTOR2(v0*cosf(angleFly), v0*sinf(angleFly)), beginPosition)
+{
+	_state = BULLET_BASE_STATE::FLYING;
+}
+
+CEnemyMachineAutoBullet::~CEnemyMachineAutoBullet()
+{
+}
+
+int CEnemyMachineAutoBullet::Initlize()
+{	
+	_origin = D3DXVECTOR2(_size.x / 2, _size.y / 2);
+	this->UpdateBox();
+	return 1;
+}
+
+void CEnemyMachineAutoBullet::Update(CTimer* gameTime)
+{
+	if (_state == BULLET_BASE_STATE::FLYING)
+	{
+		_position.x += gameTime->GetTime()*_v.x;
+		_position.y += gameTime->GetTime()*_v.y;
+	}
+
+	this->UpdateBox();
+}
+
+void CEnemyMachineAutoBullet::UpdateBox()
+{
+	_box._x = _position.x - _sprite.GetFrameWidth() / 2;
+	_box._y = _position.y + _sprite.GetFrameHeight() / 2;
+	_box._vx = _v.x;
+	_box._vy = _v.y;
+	_box._width = _sprite.GetFrameWidth();
+	_box._height = _sprite.GetFrameHeight();
+}
+
+void CEnemyMachineAutoBullet::Render(CTimer* gameTime, MGraphic* graphics)
+{
+	if (_state == BULLET_BASE_STATE::FLYING)
+		graphics->Draw(_sprite.GetTexture(), _sprite.GetDestinationRectangle(), _position, true);
+
+	CGameObject::Render(gameTime, graphics);
+}
+
+void CEnemyMachineAutoBullet::OnCollideWith(CGameObject* gameObj, CDirection normalX, CDirection normalY, float deltaTime)
+{
+	
+}
